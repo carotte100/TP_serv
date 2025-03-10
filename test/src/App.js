73276ed40f6ec08960145133575e975d53fda46e
carotte100 <carottelenullos@gmail.com>
2025-03-10 13:40:53 +0100

    suppr css

--- test/src/App.js
+++ test/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div style={styles.container}>
+    <div>
       <h1>Bienvenue dans mon projet React ! 🚀</h1>
       <p>Voila mon boutton.</p>
       <MyButton />
@@ -16,17 +16,5 @@ function MyButton() {
     <button>Je suis un bouton</button>
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#f4f4f4',
-    fontFamily: 'Arial, sans-serif',
-  },
-};
 
 export default App;
