a627e63f0ae44d2173c01aad5d968aae56f13cb1
carotte100 <carottelenullos@gmail.com>
2025-03-10 12:56:41 +0100

    premier test

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>TP (Maxime, Mehdi)</title>
+</head>
+
+<body>
+    <p>Voici ma page</p>
+</body>
+
+</html>
+
+<?php
+
+
+?>
