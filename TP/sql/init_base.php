7c572686052427c82fa92ac6d82d97659feee471
Layore3 <147826868+Layore3@users.noreply.github.com>
2025-03-14 15:49:34 +0100

    creation winton sans bullshit

--- TP/sql/init_base.php
+++ TP/sql/init_base.php
@@ -1,8 +1,5 @@
 <?php
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE winton(
     id INT NOT NULL,
     nom VARCHAR(100) NOT NULL,
@@ -10,9 +7,5 @@ $sql = "CREATE TABLE winton(
     date_de_sortie DATE NOT NULL,
     note_moyenne DECIMAL(10, 2) NOT NULL,
     CONSTRAINT pk_winton PRIMARY KEY (id));";
- $pdo->exec($sql);
- echo "Table winton created successfully.";
-} catch (PDOException $e) {
- echo "Error: " . $e->getMessage();
-}
+
 ?>
