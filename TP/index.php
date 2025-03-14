94dcb6f5545cd5d51fe613fc976900b672e779b0
carotte100 <carottelenullos@gmail.com>
2025-03-14 14:03:01 +0100

    premier changement

--- TP/index.php
+++ TP/index.php
@@ -9,14 +9,25 @@
 </head>
 
 <body>
-    <h1>Boutique de WINTON</h1>
 
-    <?php
-    echo "<p>Date et heure actuelles : " . date('Y-m-d H:i:s') . "</p>";
-    echo "<p> It's WINTON time !!!! </p>";
-    ?>
+    <div name="header">
+        <h1>Boutique de WINTON</h1>
 
-    <h2>voici le site</h2>
+        <?php
+        echo "<p>Date et heure actuelles : " . date('Y-m-d H:i:s') . "</p>";
+        echo "<p> It's WINTON time !!!! </p>";
+        ?>
+
+        <h2>voici le site</h2>
+    </div>
+
+    <div name="content">
+
+    </div>
+
+    <div name="footer">
+        <p> Mention légales blabla OVERWAT </p>
+    </div>
 </body>
 
 </html>
