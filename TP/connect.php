b4d3f1052d880c31ad436ed2274709a605de4268
carotte100 <carottelenullos@gmail.com>
2025-04-05 23:16:18 +0200

    etape 3

--- TP/connect.php
+++ TP/connect.php
@@ -1,5 +1,5 @@
 <?php
-define('SERVEUR_BD', '172.17.0.3');
+define('SERVEUR_BD', 'c_mariadb');
 define('LOGIN_BD', 'userbd');  // Utilisateur défini dans Docker
 define('PASS_BD', 'passwordbd');  // Mot de passe défini dans Docker pour userbd
 define('NOM_BD', 'tp_serveur');  // Nom de la base de données
