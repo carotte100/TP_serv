19ed41d5c7f99ad104c1156615b78d2d42ebbc67
carotte100 <carottelenullos@gmail.com>
2025-04-09 19:49:48 +0200

    aie

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-define('SERVEUR_BD', '172.17.0.3');
+define('SERVEUR_BD', 'tp_serv-mariadb-1');
 define('LOGIN_BD', 'userbd');  // Utilisateur défini dans Docker
 define('PASS_BD', 'passwordbd');  // Mot de passe défini dans Docker pour userbd
 define('NOM_BD', 'tp_serveur');  // Nom de la base de données
