264c767bb68d993c5008cb4a2e60a085366e664d
carotte100 <carottelenullos@gmail.com>
2025-04-09 19:52:34 +0200

    mauvais mdp

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 define('SERVEUR_BD', 'tp_serv-mariadb-1');
 define('LOGIN_BD', 'userbd');  // Utilisateur défini dans Docker
-define('PASS_BD', 'passwordbd');  // Mot de passe défini dans Docker pour userbd
+define('PASS_BD', 'merdi');  // Mot de passe défini dans Docker pour userbd
 define('NOM_BD', 'tp_serveur');  // Nom de la base de données
 ?>
