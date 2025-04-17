6bad0d81a2c30673b239c1314299985189c1d743
carotte100 <carottelenullos@gmail.com>
2025-04-17 19:44:47 +0200

    si c'est ça

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 define('SERVEUR_BD', 'mariadb');
-define('LOGIN_BD', 'userbd');  // Utilisateur défini dans Docker
+define('LOGIN_BD', 'userdb');  // Utilisateur défini dans Docker
 define('PASS_BD', 'merdi');  // Mot de passe défini dans Docker pour userbd
 define('NOM_BD', 'tp_serveur');  // Nom de la base de données
 ?>
